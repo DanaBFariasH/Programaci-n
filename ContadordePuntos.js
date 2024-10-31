9b13253d99ec6fc602c79ed0d64b87958a84e006
DanaBFariasH <132923486+DanaBFariasH@users.noreply.github.com>
2024-10-31 09:43:30 -0300

    Cambios

--- ContadordePuntos.js
+++ ContadordePuntos.js
@@ -1,38 +1,37 @@
-var equipo1 = {
+var tr = {
     punto: 0
 };
-var equipo2 = {
+var tn = {
     punto: 0
 };
-var handball_tr = document.getElementById('handball_Tr');
-var resistencia_tr = document.getElementById('resistencia_Tr');
+var handball_tr = document.getElementById('handball_tr');
+var resistencia_tr = document.getElementById('resistencia_tr');
 var ajedrez_tr = document.getElementById('ajedrez_tr');
-var handball_tn = document.getElementById('handball_TN ');
-var resistencia_tn = document.getElementById('esistencia_TN');
-var ajedrez_tn = document.getElementById('ajedrez_TN ');
-var Puntosequipo1 = document.getElementById('PuntosTribuRoja');
-var PuntosTribuNegra = document.getElementById('PuntosTribuNegra');
+var handball_tn = document.getElementById('handball_tn');
+var resistencia_tn = document.getElementById('resistencia_tn');
+var ajedrez_tn = document.getElementById('ajedrez_tn');
+var OutputPuntosTribuRoja = document.getElementById('PuntosTribuRoja');
+var OutputPuntosTribuNegra = document.getElementById('PuntosTribuNegra');
 var TribuGanadora = document.getElementById('TribuGanadora');
-function AgregarPuntos(tribu) {
-    if (tribu == "equipo1") {
-        var valor_A_Ingresado_Handball = parseInt(handball_tr.value, 10);
-        var valor_A_Ingresado_Resistencia = parseInt(resistencia_tr.value, 10);
-        var valor_A_Ingresado_Ajedrez = parseInt(ajedrez_tr.value, 10);
-        equipo1.punto += valor_A_Ingresado_Handball + valor_A_Ingresado_Resistencia + valor_A_Ingresado_Ajedrez;
-        Puntosequipo1.innerText += equipo1.punto.toString() + '-';
-    }
-    if (tribu == "equipo2") {
-        var valor_B_Ingresado_Handball = parseInt(handball_tn.value, 10);
-        var valor_B_Ingresado_Resistencia = parseInt(resistencia_tn.value, 10);
-        var valor_B_Ingresado_Ajedrez = parseInt(ajedrez_tn.value, 10);
-        equipo2.punto += valor_B_Ingresado_Handball + valor_B_Ingresado_Resistencia + valor_B_Ingresado_Ajedrez;
-        PuntosTribuNegra.innerText += equipo2.punto.toString() + '-';
-    }
+ == "equipo1";
+{
+    var valor_A_Ingresado_Handball = parseInt(handball_tr.value, 10);
+    var valor_A_Ingresado_Resistencia = parseInt(resistencia_tr.value, 10);
+    var valor_A_Ingresado_Ajedrez = parseInt(ajedrez_tr.value, 10);
+    tr.punto += valor_A_Ingresado_Handball + valor_A_Ingresado_Resistencia + valor_A_Ingresado_Ajedrez;
+    OutputPuntosTribuRoja.innerText += tr.punto.toString() + '-';
 }
-function MostrarResultados(tribu) {
-    var resultado = '';
-    if (tribu == "equipo2") {
-        equipo2.punto -= equipo2.punto;
-        PuntosTribuNegra.innerText = '';
-    }
+if (tribu == "equipo2") {
+    var valor_B_Ingresado_Handball = parseInt(handball_tn.value, 10);
+    var valor_B_Ingresado_Resistencia = parseInt(resistencia_tn.value, 10);
+    var valor_B_Ingresado_Ajedrez = parseInt(ajedrez_tn.value, 10);
+    tn.punto += valor_B_Ingresado_Handball + valor_B_Ingresado_Resistencia + valor_B_Ingresado_Ajedrez;
+    OutputPuntosTribuNegra.innerText += tn.punto.toString() + '-';
+}
+var resultado = '';
+if (tribu === "Tribu Roja") {
+    alert("Puntaje actual de la Tribu Roja:" + tr.punto);
+}
+else if (tribu === "Tribu Negra") {
+    alert("Puntaje actual de la Tribu Negra:" + tn.punto);
 }
--- ContadordePuntos.ts
+++ ContadordePuntos.ts
@@ -1,53 +1,63 @@
-interface punto{
-    punto:number;
+interface puntos {
+    punto : number;
 }
-let equipo1: punto={
-    punto:0
-}
-let equipo2: punto={
-    punto:0
-}
+let tr: puntos = {
+    punto : 0
+ };
+ let tn: puntos = {
+    punto : 0
+ };
 
 const handball_tr = document.getElementById('handball_tr') as HTMLInputElement;
-const resistencia_tr = document.getElementById('resistencia_Tr') as HTMLInputElement;
+const resistencia_tr = document.getElementById('resistencia_tr') as HTMLInputElement;
 const ajedrez_tr = document.getElementById('ajedrez_tr') as HTMLInputElement;
 
-const handball_tn = document.getElementById('handball_tn ') as HTMLInputElement;
-const resistencia_tn = document.getElementById('esistencia_tn') as HTMLInputElement;
-const ajedrez_tn = document.getElementById('ajedrez_tn ') as HTMLInputElement;
+const handball_tn = document.getElementById('handball_tn') as HTMLInputElement;
+const resistencia_tn = document.getElementById('resistencia_tn') as HTMLInputElement;
+const ajedrez_tn = document.getElementById('ajedrez_tn') as HTMLInputElement;
 
-const Puntosequipo1 = document.getElementById('PuntosTribuRoja') as HTMLElement;
-const PuntosTribuNegra = document.getElementById('PuntosTribuNegra') as HTMLElement;
+const OutputPuntosTribuRoja = document.getElementById('PuntosTribuRoja') as HTMLElement;
+const OutputPuntosTribuNegra = document.getElementById('PuntosTribuNegra') as HTMLElement;
 
 const TribuGanadora = document.getElementById('TribuGanadora') as HTMLElement;
 
-function AgregarPuntos(tribu : string): void {
 
-    if(tribu == "equipo1"){
+function AgregarPuntos(equipo : string){
+
+    if(equipo === 'equipo1'){
         let valor_A_Ingresado_Handball : number = parseInt(handball_tr.value, 10);
         let valor_A_Ingresado_Resistencia : number = parseInt(resistencia_tr.value,10);
         let valor_A_Ingresado_Ajedrez : number = parseInt(ajedrez_tr.value,10);
-        equipo1.punto += valor_A_Ingresado_Handball + valor_A_Ingresado_Resistencia + valor_A_Ingresado_Ajedrez
-        Puntosequipo1.innerText += equipo1.punto.toString() +'-';
+        tr.punto += valor_A_Ingresado_Handball + valor_A_Ingresado_Resistencia + valor_A_Ingresado_Ajedrez
+        //OutputPuntosTribuRoja.innerText += tr.punto.toString() +'-';
     }
-    if(tribu == "equipo2"){
+    if(equipo === 'equipo2'){
         let valor_B_Ingresado_Handball : number = parseInt(handball_tn.value,10);
         let valor_B_Ingresado_Resistencia : number = parseInt(resistencia_tn.value,10);
         let valor_B_Ingresado_Ajedrez : number = parseInt(ajedrez_tn.value,10);
-        equipo2.punto += valor_B_Ingresado_Handball + valor_B_Ingresado_Resistencia + valor_B_Ingresado_Ajedrez
-        PuntosTribuNegra.innerText += equipo2.punto.toString() +'-';
+        tn.punto += valor_B_Ingresado_Handball + valor_B_Ingresado_Resistencia + valor_B_Ingresado_Ajedrez
+        //OutputPuntosTribuNegra.innerText += tn.punto.toString() +'-';
 
     }
 }
-function MostrarResultado(): void{
-    let Resultado = '';
-    if (equipo1.punto > equipo2.punto){
-      Resultado = 'La Tribu Roja es gano con' + equipo1.punto + 'puntos';
-    } else if   (equipo1.punto < equipo2.punto){
-        Resultado = 'La Tribu Negra gano con' + equipo2.punto + 'puntos';
-    } else{
-        Resultado = 'Ambos equipos estan empatados con' + equipo1.punto + 'puntos';
-    }
-    Resultado.innerText= Resultado; 
+//function MostrarResultados(AgregarPuntos){
+  //  if(tribu === "Tribu Roja"){
+    //    alert("Puntaje actual de la Tribu Roja:" + tr.punto);
+    
+    //} else if (tribu=== "Tribu Negra"){
+      //  alert(" Puntaje actual de la Tribu Negra"+ tn.punto);
+    //}
+    //TribuGanadora.innerText= 
+      
+//}
+function MostrarResultados(){
+  let Resultado = '';
+  if (tr.punto > tn.punto){
+    Resultado = 'Tribu Roja es el ganador con' + tr.punto + ' puntos';
+  } else if   (tr.punto < tn.punto){
+      Resultado = 'Tribu Negra es el ganador con' + tn.punto + ' puntos';
+  } else{
+      Resultado = 'Ambos equipos estan ematados con' + tr.punto + ' puntos';
   }
-
+  TribuGanadora.innerText= Resultado; 
+}
